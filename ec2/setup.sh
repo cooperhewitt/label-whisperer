2774b2ce9c954bc6f0dabdd1f5f415bd1ce84d93
straup <copea@si.edu>
2014-01-23 15:36:55 +0000

    add ec2/setup.sh

---
+++ ec2/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install sysstat htop unzip
+sudo apt-get install tcsh emacs24-nox 
+
+sudo apt-get install git tesseract-ocr python-setuptools
+
+sudo easy_install flask
